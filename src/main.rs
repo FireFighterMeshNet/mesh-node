3e9d0fe78b5fa255e44722167f7822b7cf570d8c
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2024-09-19 01:04:30 -0600

    initial working repo, detect button, alloc, terminal output, panic all working

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use alloc::{vec, vec::Vec};
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl,
+    delay::Delay,
+    gpio::{Input, Io},
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemControl,
+};
+
+#[entry]
+fn main() -> ! {
+    // Provides #[global_allocator] with given number of bytes.
+    esp_alloc::heap_allocator!(1024);
+
+    let peripherals = Peripherals::take();
+    let system = SystemControl::new(peripherals.SYSTEM);
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let delay = Delay::new(&clocks);
+
+    esp_println::logger::init_logger_from_env();
+
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let button = Input::new(io.pins.gpio0, esp_hal::gpio::Pull::None);
+
+    let mut v: Vec<u8> = vec![0; 256];
+    let mut i = 0;
+    loop {
+        log::info!("Hello world!");
+        if button.is_high() {
+            log::info!("hi")
+        } else if button.is_low() {
+            log::info!("lo")
+        }
+        log::info!("v: {:?}", v.len());
+        delay.delay(50.millis());
+        v.push(i);
+        i += 1;
+    }
+}
